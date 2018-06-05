ed79e3e1361f6426c27919e873839d87495b8e2b
Amey Naiyan <ameyd9@gmail.com>
2018-06-05 12:58:21 -0400

    Added Views

--- adn-snake/src/app/app.module.ts
+++ adn-snake/src/app/app.module.ts
@@ -4,11 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './view/menu/menu.component';
+import { NewComponent } from './view/new/new.component';
+import { PlayComponent } from './view/play/play.component';
+import { ScoresComponent } from './view/scores/scores.component';
+import { FinishComponent } from './view/finish/finish.component';
+import { HeaderComponent } from './common/header/header.component';
+import { BackdropComponent } from './common/backdrop/backdrop.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    NewComponent,
+    PlayComponent,
+    ScoresComponent,
+    FinishComponent,
+    HeaderComponent,
+    BackdropComponent
   ],
   imports: [
     BrowserModule,
